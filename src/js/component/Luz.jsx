d3913db89b6647d9916d75689cf723a6a4094548
4lvaro8 <ponitsa@gmail.com>
2024-07-18 09:08:45 +0000

    final

--- src/js/component/Luz.jsx
+++ src/js/component/Luz.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Button from "./buttons"
 
 
 
@@ -7,21 +8,32 @@ import { useState } from "react";
 //create your first component
 const Luz = () => {
 	const [select, setSelect] = useState("")
+	const [opened, setOpened] = useState(false);
 
+	/** Cambia el color seleccionado
+	 * 
+	 * @param {color} string
+	 */
 	function ChangeColor(color) {
 		setSelect(color)
 	}
 
+
+	/** Alterna los colores del semáforo
+	 * 
+	 */
 	function nextColor() {
 		select === "redLight"
 			? setSelect("yellowLight")
 			: select === "yellowLight"
 				? setSelect("greenLight")
-				: setSelect("redLight")
+				: select === ("greenLight")
+					? setSelect("purpleLight")
+					: setSelect("redLight")
 	}
 
 
-	return (
+	return !opened ? (
 		<>
 			<div className="semaforo">
 				<div className={`redLight ${select == "redLight" ? "lightR" : ""}`}
@@ -37,7 +49,41 @@ const Luz = () => {
 				></div>
 			</div>
 
-			<button className="btn" onClick={nextColor}>Siguiente Color</button>
+			<Button
+				btnAlternate={nextColor}
+				btnPurple={() => setOpened(true)}
+			/>
+		</>
+	) : (
+		<>
+			<div className="semaforo">
+
+				<div className={`redLight ${select == "redLight" ? "lightR" : ""}`}
+					onClick={() => ChangeColor("redLight")}
+				></div>
+
+				<div className={`yellowLight ${select == "yellowLight" ? "lightY" : ""}`}
+					onClick={() => ChangeColor("yellowLight")}
+				></div>
+
+				<div className={`greenLight ${select == "greenLight" ? "lightG" : ""}`}
+					onClick={() => ChangeColor("greenLight")}
+				></div>
+
+				<div className={`purpleLight ${select == "purpleLight" ? "lightP" : ""}`}
+					onClick={() => ChangeColor("purpleLight")}
+				></div>
+			</div>
+
+			<Button
+				btnAlternate={nextColor}
+				btnPurple={() => setOpened(false)}
+			/>
+
+			<div className="alert alert-info d-flex justify-content-center" role="alert">
+				<strong>Cuidado!</strong> Que viene el morado.
+			</div>
+
 		</>
 	);
 };
