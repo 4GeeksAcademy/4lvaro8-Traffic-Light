fe4ae527a2ae7d284a4c2b61a112bf7ecf909d38
4lvaro8 <ponitsa@gmail.com>
2024-07-22 20:57:05 +0000

    cambios

--- src/js/component/Luz.jsx
+++ src/js/component/Luz.jsx
@@ -1,10 +1,4 @@
-
-
-import React from "react";
-import { useState } from "react";
-
-
-
+import React, { useState } from "react";
 
 
 //create your first component
@@ -18,35 +12,35 @@ const Luz = () => {
 	 * 
 	 */
 	function nextColor() {
-		select === "red"
-			? setSelect("yellow")
-			: select === "yellow"
-				? setSelect("green")
-				: select === ("green")
-					? setSelect("purple")
-					: setSelect("red")
+		select === colors[0]
+			? setSelect(colors[1])
+			: select === colors[1]
+				? setSelect(colors[2])
+				: select === (colors[2])
+					? setSelect(colors[3])
+					: setSelect(colors[0])
 	}
 
 
 	return (
 		<>
 			<div className="semaforo">
-				<div className={`red light ${select == "red" ? "active" : ""}`} onClick={() => setSelect("red")}></div>
+				<div className={`red light ${select == "red" ? "active" : ""}`} onClick={() => setSelect(colors[0])}></div>
 
-				<div className={`yellow light ${select == "yellow" ? "active" : ""}`} onClick={() => setSelect("yellow")}></div>
+				<div className={`yellow light ${select == "yellow" ? "active" : ""}`} onClick={() => setSelect(colors[1])}></div>
 
-				<div className={`green light ${select == "green" ? "active" : ""}`} onClick={() => setSelect("green")}></div>
+				<div className={`green light ${select == "green" ? "active" : ""}`} onClick={() => setSelect(colors[2])}></div>
 
-				{isOpened && <div className={`purple light ${select == "purple" ? "active" : ""}`} onClick={() => setSelect("purple")}></div>}
+				{isOpened && <div className={`purple light ${select == "purple" ? "active" : ""}`} onClick={() => setSelect(colors[3])}></div>}
 
 			</div>
 
 			<div className="buttons-container">
 				<button className="btnAlternate" onClick={nextColor}>Siguiente Color</button>
-				<button className="btnPurple" onClick={() => isOpened ? setIsOpened(false) : setIsOpened(true) }>Semáforo morado</button>
+				<button className="btnPurple" onClick={() => isOpened ? setIsOpened(false) : setIsOpened(true)}>Semáforo morado</button>
 			</div>
 		</>
-	) 
+	)
 };
 
 export default Luz;
